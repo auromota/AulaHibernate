9f10a592a7c76038d14c4a5cfa65564a7bbec349
Auro Mota <palubo01@gmail.com>
2016-04-27 21:32:12 -0300

    Adding entity manager factory

---
+++ src/main/java/br/edu/univas/bd/AulaHibernate/HibernateUtil.java
@@ -0,0 +1,23 @@
+package br.edu.univas.bd.AulaHibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+	private static final EntityManagerFactory emFactory = buildSessionFactory();
+
+	private static EntityManagerFactory buildSessionFactory() {
+		try {
+			return Persistence.createEntityManagerFactory("PERSISTENCE_CTX_NAME");
+		} catch (Throwable e) {
+			System.err.println("Error creating EntityManagerFactory." + e);
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	public static EntityManager getEntityManager() {
+		return emFactory.createEntityManager();
+	}
+}
